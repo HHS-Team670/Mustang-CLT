4b9c81d3120dd30655cf06ab5a4a00568fec7d5e
Laksh Bhambhani <lakshbh@gmail.com>
2020-08-16 03:10:56 -0700

    documented logfinder.bat and updated readme

--- logfinder.bat
+++ logfinder.bat
@@ -1,3 +1,10 @@
+REM Used to scp the latest log file to the laptop and echo certain tokens to a csv. 
+
+REM ssh's onto the robot and sets LOGFILE to the path of the latest log file
+REM scp the logfile to the laptop and echo's the variable names to a csv files
+REM finally, the specific tokens mentioned in the command are echoed to the same csv file
+REM Later, a graphing software could be used to graph the csv
+
 ssh admin@10.6.70.2 "ls -tr /home/lvuser/*txt | tail -1" | find "lvuser" > %TEMP%%\logpath.txt
 set /p LOGFILE=<%TEMP%\logpath.txt
 del %TEMP%\logpath.txt
