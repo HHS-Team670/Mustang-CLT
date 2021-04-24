3fddf6837b9a833566a00fe5a8fda122d4dd587b
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-04-24 11:49:50 -0700

    change token values for 2020 robot

--- logfinder.bat
+++ logfinder.bat
@@ -10,4 +10,4 @@ set /p LOGFILE=<%TEMP%\logpath.txt
 del %TEMP%\logpath.txt
 scp admin@10.6.70.2:%LOGFILE% latestlog.txt
 echo "LeftSpeedSetPoint, LeftSpeed, RightSpeedSetPoint, RightSpeed"> testdata.csv 
-(for /f "tokens=8,11,15,18" %%i in ('find "SetPoint" latestlog.txt') do @echo %%i %%j %%k, %%l) >> testdata.csv
+(for /f "tokens=9,13,16,18" %%i in ('find "SetPoint" latestlog.txt') do @echo %%i %%j %%k, %%l) >> testdata.csv
